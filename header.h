924e2bba0d8067a283ad76625d5e9408ec3a41a9
VanSHOE <75690289+VanSHOE@users.noreply.github.com>
2021-04-18 21:00:48 +0530

    Re-added some removed comments

--- header.h
+++ header.h
@@ -29,7 +29,11 @@ void toss();
 
 //update board
 game_state move_entries(pawn p); // Function to move one of the pawns
-
+/*
+in this function i am writing here but after .c is made paste it there,
+we have to update board which involves lots of subfunctions like checking if move is legal or not
+also we have to update the position. and additional features if one can add (using \b) sounds to show moves
+*/
 void print_board(game_state P);
 game_state undo(pawn p);
 void rule();                        //just prints rule book
@@ -37,6 +41,11 @@ bool islegal(pawn p, pawn new_pos); // Need a 'from' and a 'to'
 void result(game_state P);          //tells the result of the game
 
 //interface
+/*
+we have to make elegant impresion and to fully appriciate the function print board, we have to create 
+a game like interface for board. apart from that we also want function to show legal moves
+i cant think of functions data types so plz excuse me.....
+*/
 
 //analysis
 void review();                              //slideshow of board , added:- we will be using a global stack , no need to pass anything in review, undo etc
